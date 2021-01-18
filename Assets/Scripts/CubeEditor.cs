69327382eafb314e5925aa5ab4965c8c6cabf1a0
DaisyDoesDev <69629996+DaisyDoesDev@users.noreply.github.com>
2021-01-18 21:35:09 +0200

    Initial Commit

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f) ]float gridSize = 10f;
+
+    TextMesh coordLabel;
+    Vector3 snapPos;
+
+    void Start()
+    {
+        coordLabel = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        snapCubeToGrid();
+        setCoordLabel();
+        setGameObjectName();
+    }
+
+    void setCoordLabel()
+    {
+        coordLabel.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
+
+    void setGameObjectName()
+    {
+        gameObject.name = "Cube: (" + snapPos.x / gridSize + "," + snapPos.z / gridSize + ")";
+    }
+
+    void snapCubeToGrid()
+    {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        printAllBlocks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void printAllBlocks()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            print(waypoint.name);
+        }
+    }
+}
